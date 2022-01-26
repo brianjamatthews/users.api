ef54fa2d357a33b144fe948ced7087450de8dd38
Brian Matthews <brianjamesmatthews@gmail.com>
2022-01-25 17:23:34 -0800

    Add users db context

--- src/User.Api/Program.cs
+++ src/User.Api/Program.cs
@@ -1,6 +1,15 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using Users.Infrastructure.Data;
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<UsersDbContext>(
+      options => options.UseCosmos(
+          builder.Configuration["Cosmos:ConnectionString"],
+          builder.Configuration["Cosmos:DatabaseName"]));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ src/Users.Infrastructure/Data/UsersDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Users.Infrastructure.Data;
+
+/// <summary>
+/// Users db context
+/// </summary>
+public class UsersDbContext : DbContext
+{
+    /// <summary>
+    /// Instantiates a <see cref="UsersDbContext"/>
+    /// </summary>
+    /// <param name="options">The <see cref="DbContextOptions{TContext}"/></param>
+    public UsersDbContext(DbContextOptions<UsersDbContext> options)
+        : base(options)
+    {
+    }
+}
