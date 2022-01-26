832e4c82480e3eac8d708ebee24e17bc3516cfdc
Brian Matthews <brianjamesmatthews@gmail.com>
2022-01-26 13:35:42 -0800

    Update to intialize db on startup

--- src/Users.Api/Program.cs
+++ src/Users.Api/Program.cs
@@ -1,16 +1,30 @@
 ﻿using System.Reflection;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Users.ApplicationCore.Commands;
 using Users.ApplicationCore.Interfaces;
+using Users.ApplicationCore.Profiles;
 using Users.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Initialize DB
+var options = new DbContextOptionsBuilder<UsersDbContext>()
+    .UseCosmos(
+        builder.Configuration["Cosmos:ConnectionString"],
+        builder.Configuration["Cosmos:DatabaseName"])
+    .Options;
+
+using (var context = new UsersDbContext(options))
+{
+    await context.Database.EnsureCreatedAsync();
+}
+
 builder.Services.AddControllers();
 
-builder.Services.AddMediatR(typeof(Program).GetTypeInfo().Assembly);
+builder.Services.AddMediatR(typeof(CreateUserCommand).GetTypeInfo().Assembly);
 
-builder.Services.AddAutoMapper(typeof(Program).GetTypeInfo().Assembly);
+builder.Services.AddAutoMapper(typeof(UserProfile).GetTypeInfo().Assembly);
 
 builder.Services.AddDbContext<IUsersDbContext, UsersDbContext>(
       options => options.UseCosmos(
