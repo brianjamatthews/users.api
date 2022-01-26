be74d5f3550946d88e9eb2042e12cb2aa61ffdbf
Brian Matthews <brianjamesmatthews@gmail.com>
2022-01-26 11:58:35 -0800

    Add user read model

---
+++ src/Users.ApplicationCore/Models/UserReadModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Users.ApplicationCore.Models;
+
+/// <summary>
+/// User read model
+/// </summary>
+/// <param name="id">Unique identifer</param>
+/// <param name="firstName">First name</param>
+/// <param name="middleName">Middle name</param>
+/// <param name="lastName">Last name</param>
+/// <param name="phoneNumber">Phone number</param>
+/// <param name="emailAddress">Email address</param>
+public record UserReadModel(
+    Guid id,
+    string firstName,
+    string? middleName,
+    string lastName,
+    string? phoneNumber,
+    string? emailAddress)
+{
+    /// <summary>
+    /// Full name
+    /// </summary>
+    public string Name => $"{firstName} {middleName} {lastName}".Trim();
+}
