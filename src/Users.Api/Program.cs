3dc537437814f12c6f3a79ffd37795da83ce9772
Brian Matthews <brianjamesmatthews@gmail.com>
2022-01-26 13:20:29 -0800

    Add xml comments to swagger gen

--- src/Users.Api/Program.cs
+++ src/Users.Api/Program.cs
@@ -18,7 +18,11 @@ builder.Services.AddDbContext<IUsersDbContext, UsersDbContext>(
           builder.Configuration["Cosmos:DatabaseName"]));
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
