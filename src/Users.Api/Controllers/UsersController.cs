6fe6e864ec0e06f2aa354b87a11ae848949040b3
Brian Matthews <brianjamesmatthews@gmail.com>
2022-01-26 16:43:22 -0800

    Add get user endpoint

--- src/Users.Api/Controllers/UsersController.cs
+++ src/Users.Api/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
-﻿using System.Net.Mime;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Mime;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Users.ApplicationCore.Commands;
 using Users.ApplicationCore.Models;
+using Users.ApplicationCore.Queries;
 
 namespace Users.Api.Controllers;
 
@@ -51,6 +53,36 @@ public class UsersController : ControllerBase
         CancellationToken cancellationToken = default)
     {
         var user = await _mediater.Send(command, cancellationToken);
-        return Created("", user);
+        return CreatedAtRoute(nameof(GetUser), new { emailAddress = user.emailAddress }, user);
+    }
+
+    /// <summary>
+    /// Gets a user by email address
+    /// </summary>
+    /// <param name="emailAddress">The users email address to search by</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/></param>
+    /// <returns>The user</returns>
+    /// <response code="200">Returns the newly created user</response>
+    /// <response code="400">If the request is bad</response>
+    /// <response code="404">If the user isn't found</response>
+    [HttpGet(Name = nameof(GetUser))]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<UserReadModel>> GetUser(
+        [EmailAddress]
+        [StringLength(256)]
+        string emailAddress,
+        CancellationToken cancellationToken = default)
+    {
+        var query = new GetUserQuery(emailAddress);
+        var user = await _mediater.Send(query, cancellationToken);
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return user;
     }
 }
